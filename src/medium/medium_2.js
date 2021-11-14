6324387b39c99aac3b15de773f2e42b3e67b33fa
icadeal <calebrd@live.unc.edu>
2021-11-14 17:13:35 -0500

    corrected key formatting when accessing dictionary indices medium_2

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -24,8 +24,8 @@ see under the methods section
     var sumCity = 0
     var sumHighway = 0
     for (var i = 0; i < mpg_data.length; i++) {
-        sumCity += mpg_data[i['city']]
-        sumHighway += mpg_data[i['highway']]
+        sumCity += mpg_data[i[city]]
+        sumHighway += mpg_data[i[highway]]
     }
     var averageCity = sumCity / mpg_data.length
     var averageHighway = sumHighway / mpg_data.length
@@ -36,7 +36,7 @@ see under the methods section
 function allYearStats() {
     var yearsArray = []
     for (var i = 0; i < mpg_data.length; i++) {
-        yearsArray.push(mpg_data[i["year"]])
+        yearsArray.push(mpg_data[i[year]])
     }
     return getStatistics(yearsArray)
 }
@@ -45,7 +45,7 @@ function ratioHybrids() {
     var hybrids = 0
     var total = 0
     for (var i =0; i <mpg_data.length; i++) {
-        if (mpg_data[i["hybrid"]] == true) {
+        if (mpg_data[i[hybrid]] == true) {
             hybrids += 1
         } 
         total += 1
