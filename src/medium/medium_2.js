3f56362e92d3fe2d2deae961cd45422c9a0a69f2
icadeal <calebrd@live.unc.edu>
2021-11-15 10:20:27 -0500

    tried yet again allCarStats, returning NaN idk why

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -19,39 +19,43 @@ see under the methods section
  *
  * @param {allCarStats.ratioHybrids} ratio of cars that are hybrids
  */
+export function avgMpg() {
+    var sumCity = 0
+    var sumHighway = 0
+    for (var i = 0; i < mpg_data.length; i++) {
+        sumCity += mpg_data[i["city"]]
+        sumHighway += mpg_data[i["highway"]]
+    }
+    var averageCity = sumCity / mpg_data.length
+    var averageHighway = sumHighway / mpg_data.length
+    var dictionary = {city: averageCity, highway: averageHighway}
+    return dictionary
+}
 
+export function allYearStats() {
+    var yearsArray = []
+    for (var i = 0; i < mpg_data.length; i++) {
+        yearsArray.push(mpg_data[i["year"]])
+    }
+    return getStatistics(yearsArray)
+}
+
+export function ratioHybrids() {
+    var hybrids = 0
+    var total = 0
+    for (var i =0; i <mpg_data.length; i++) {
+        if (mpg_data[i["hybrid"]] == true) {
+            hybrids += 1
+        } 
+        total += 1
+    }
+    return hybrids / total 
+}
 
 export const allCarStats = {
-    avgMpg: () => {
-        var sumCity = 0
-        var sumHighway = 0
-        for (var i = 0; i < mpg_data.length; i++) {
-            sumCity += mpg_data[i["city"]]
-            sumHighway += mpg_data[i["highway"]]
-        }
-        var averageCity = sumCity / mpg_data.length
-        var averageHighway = sumHighway / mpg_data.length
-        var dictionary = {city: averageCity, highway: averageHighway}
-        return dictionary
-    },
-    allYearStats: () => {
-        var yearsArray = []
-        for (var i = 0; i < mpg_data.length; i++) {
-            yearsArray.push(mpg_data[i["year"]])
-        }
-        return getStatistics(yearsArray)
-    },
-    ratioHybrids: () => {
-        var hybrids = 0
-        var total = 0
-        for (var i =0; i <mpg_data.length; i++) {
-            if (mpg_data[i["hybrid"]] == true) {
-                hybrids += 1
-            } 
-            total += 1
-        }
-        return hybrids / total
-    },
+    avgMpg: avgMpg(),
+    allYearStats: allYearStats(),
+    ratioHybrids: ratioHybrids(),
 };
 
 
